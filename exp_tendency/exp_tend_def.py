b798aa3f4df8b8305b7df6c9411a50c6e23772c8
Ke <kesang0156357@gmail.com>
2015-05-10 12:11:16 -0400

    add to-do-list

--- exp_tendency/exp_tend_def.py
+++ exp_tendency/exp_tend_def.py
@@ -1,4 +1,12 @@
-
+'''
+#to-do-list:
+1)get the reported threshold for each category
+??5)(Maybe you can also show all of the individual threshold lines for each subject in each of the four categories, too, so we can see how well the mean captures all the individual thresholds....) 
+2)get modelB threshold for each category:mean/median
+3)get the # of subjects best fitting for each type of model
+4)get the # of subjects besting fitting in each of the 4 category for each type of model
+6)compare the 4 reported threshold with other fitting modeled threshold, like the constant model?
+'''
 # find the tendency for each subj as the following policy:
 # If the reported thresholds at those 6 turns remained the same, participants were classified as Constant;
 # if the values increased at least once and never decreased, participants were classified as Increasing;
@@ -45,7 +53,7 @@ def  match(row):
 df.loc[:, 'class'] = df.apply(match, axis = 1)
 
 class_df = df.groupby('class').agg('count')
-class_df.to_csv('class_df.csv', index = False)
+#class_df.to_csv('class_df.csv', index = True)
 
 
 
